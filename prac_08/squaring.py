317a90db4695fec2da9f6fa4f77b1b7f7514b747
Mikeeegxj <xiaojun.guo@my.jcu.edu.au>
2024-11-11 16:45:17 +0800

    completed squaring.py and squaring.kv

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -1,7 +1,5 @@
 
 
-
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
@@ -13,10 +11,12 @@ class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
+        Window.size = (300, 100)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
+
+
 
     def handle_calculate(self, value):
         """ handle calculation (could be button press or other call), output result to label widget """
@@ -24,7 +24,7 @@ class SquareNumberApp(App):
             result = float(value) ** 2
             self.root.ids.output_label.text = str(result)
         except ValueError:
-            pass
+            self.root.ids.output_label.text = 'Please enter a valid number'
 
 
 SquareNumberApp().run()
